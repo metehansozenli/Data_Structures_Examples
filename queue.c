88b096b241a6d5f324b0b5026503bdf82c1a47c7
Metehan SÖZENLİ <92057823+metehansozenli@users.noreply.github.com>
2022-12-08 03:17:16 +0300

    Update queue.c

--- queue.c
+++ queue.c
@@ -77,6 +77,8 @@ void displayQueue(QUEUE* q){
 int main()
 {
     QUEUE * q1 = createQueue();
+    QUEUE * q1 = createQueue();
+    QUEUE * q1 = createQueue();
 
     enqueue(q1,2);
     enqueue(q1,234);
